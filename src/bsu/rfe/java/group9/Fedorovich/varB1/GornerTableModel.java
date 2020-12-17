0d81ba6db75c9f1a6f060443cb7c9e5d073e1f22
Gnarll <vadimfedorovich22@mail.ru>
2020-12-17 19:35:38 +0300

    GornerTableModel

---
+++ src/bsu/rfe/java/group9/Fedorovich/varB1/GornerTableModel.java
@@ -0,0 +1,107 @@
+package bsu.rfe.java.group9.Fedorovich.varB1;
+
+
+import javax.swing.table.AbstractTableModel;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+public class GornerTableModel  extends AbstractTableModel
+{
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+    private double result[] = new double[3];
+    public GornerTableModel(Double from, Double to, Double step,
+                            Double[] coefficients) {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+    public Double getFrom() {
+        return from;
+    }
+    public Double getTo() {
+        return to;
+    }
+    public Double getStep() {
+        return step;
+    }
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+    public int getColumnCount()
+    {
+// В данной модели три столбца
+        return 3;
+    }
+    public int getRowCount()
+    {
+// Вычислить количество точек между началом и концом отрезка
+// исходя из шага табулирования
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+    }
+
+    public Object getValueAt(int row, int col)
+    {
+        formatter.setMaximumFractionDigits(5);
+        double x = from + step*row;
+        switch(col)
+        {
+            case 0:
+                return x;
+            case 1:
+            {
+                result[0] = 0.0;
+                for(int i = 0; i < coefficients.length; i++)
+                {
+                    result[0] += Math.pow(x, coefficients.length-1-i)*coefficients[i];
+                }
+                return result[0];
+            }
+            default:
+
+                double temp = result[0];
+                if (temp % 1 != 0)
+                    temp *= 100;
+                int temp1 = (int)temp;
+                temp1=Math.round(temp1);
+                int count = 0;
+                int result = 0;
+                while (temp1 != 0)
+                {
+                    if (temp1 % 10 == temp1 / 10 % 10)
+                    {
+                        count++;
+                        temp1 /= 10;
+                    }
+                    temp1 /= 10;
+                }
+                if (count >= 2)
+                    return true;
+                else
+                    return false;
+        }
+    }
+    public String getColumnName(int col)
+    {
+        switch (col)
+        {
+            case 0:
+// Название 1-го столбца
+                return "Значение X";
+            case 1:
+// Название 2-го столбца
+                return "Значение многочлена";
+            default:
+                return "Две пары";
+        }
+    }
+    public Class<?> getColumnClass(int col)
+    {
+        if (col == 2)
+            return Boolean.class;
+        else
+            return Double.class;
+    }
+}
